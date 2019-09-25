d14d4cd93c1a7e8ab35515e49d1a8939813f1861
Fredrik <fredrik1997@gmail.com>
2019-09-25 13:56:42 +0200

    Prøvde med testfunksjon men var vanskelig

---
+++ Project 2/functions.h
@@ -0,0 +1,20 @@
+#ifndef JACOBI_H
+#define JACOBI_H
+
+
+
+#include "main.cpp"
+#include <armadillo>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using namespace arma;
+
+int lol();
+tuple<vec, mat> eigsolver(mat A, int n);
+mat jacobi_rotate(mat A, int k, int l, int n, mat &eigvec);
+double max_offdiag(mat A, int &k, int &l, int n);
+
+
+#endif
