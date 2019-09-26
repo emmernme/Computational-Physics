bf3e637498668872f0cd0ef17e3957263782ea48
Emil <emil.broll@gmail.com>
2019-09-26 15:13:14 +0200

    Readability

--- Project 2/jacobi_rotate.cpp
+++ Project 2/jacobi_rotate.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 using namespace arma;
 
-
-//  for solving with jacobi's method
+// For solving with Jacobi's method
 mat jacobi_rotate(mat A, int k, int l, int n, mat &eigvec){
 	double s, c; // sin, cos
 
@@ -21,9 +20,9 @@ mat jacobi_rotate(mat A, int k, int l, int n, mat &eigvec){
 		tau = (A(l,l) - A(k,k))/(2*A(k,l));
 		// Not sure what this does
 		if (tau >= 0){
-			t = 1.0/(tau + sqrt(1.0+ tau*tau));
+			t = 1.0/(tau + sqrt(1.0 + tau*tau));
 		} else {
-			t = -1.0 /(-tau + sqrt(1.0+ tau*tau));
+			t = -1.0 /(-tau + sqrt(1.0 + tau*tau));
 		}
 
 		// Calculate cos, sin
@@ -35,14 +34,13 @@ mat jacobi_rotate(mat A, int k, int l, int n, mat &eigvec){
 	// Set up the new matrix and the max values (of the old matrix)
 	mat B = A;
 	B(0,0) = A(0,0);
-	B(k, k) = A(k,k)*pow(c,2)-2.0*A(k,l)*c*s + A(l,l)*pow(s,2);
-	B(l, l) = A(l,l)*pow(c,2)+2.0*A(k,l)*c*s + A(k,k)*pow(s,2);
+	B(k, k) = A(k,k)*pow(c,2) - 2.0*A(k,l)*c*s + A(l,l)*pow(s,2);
+	B(l, l) = A(l,l)*pow(c,2) + 2.0*A(k,l)*c*s + A(k,k)*pow(s,2);
 	B(k, l) = B(l, k) = 0.0;
-	// Make mat holders for our eigvecs
 
 	// Set the non-diagonal elements for each row
 	for (int i = 0; i < n; i++){
-		if (i != k && i!= l){
+		if (i != k && i != l){
 			B(i,i) = A(i,i);
 			B(i, k) = A(i,k)*c - A(i,l)*s;
 			B(i, l) = A(i,l)*c + A(i,k)*s;
@@ -50,17 +48,13 @@ mat jacobi_rotate(mat A, int k, int l, int n, mat &eigvec){
 			B(l, i) = B(i,l);
 		}
 
-   		//saving eigvec(i,k) before defining a new one
+   		// Saving eigvec(i,k) before defining a new one
     	mat e = eigvec;
-
     	e(i,k) = eigvec(i,k);
 
 		// Set the new eigenvectors
     	eigvec(i,k) = c * eigvec(i,k) - s * eigvec(i,l);
 		eigvec(i,l) = c * eigvec(i,l) + s * e(i,k);
 	}
-
-
-
 	return B;
 }
--- Project 2/max_offdiag.cpp
+++ Project 2/max_offdiag.cpp
@@ -1,19 +1,17 @@
 
-#include <iostream>
+//#include <iostream>
 #include <cmath>
 #include <armadillo>
 
 using namespace std;
 using namespace arma;
 
-
-
 // Finds the maximum value and its index in the matrix
 double max_offdiag(mat A, int &k, int &l, int n){
 	double maxarg = 0;
 	for (int i = 0; i < n; i++){
 		for (int j = 0; j < n; j++){
-			if (i != j && (fabs(maxarg) < fabs(A(i,j)))){
+			if (i != j && (maxarg < fabs(A(i,j)))){
 				maxarg = fabs(A(i,j));
 				k = i;
 				l = j;
