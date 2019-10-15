b898fd15e475f49d6467d7ff188dc4f987bbad9a
Benedicte <benedicte.allum@hotmail.com>
2019-10-15 12:19:30 +0200

    conflict???

--- Project3/int_function.cpp
+++ Project3/int_function.cpp
@@ -12,7 +12,6 @@ double int_function(double x1, double x2, double y1, double y2, double z1, doubl
   double r2 = sqrt(x2*x2 + y2*y2 + z2*z2);
   double deno = sqrt(pow((x1-x2), 2) + pow((y1-y2), 2) + pow((z1-z2), 2));
   double value = exp(-2*alpha*(r1 + r2)) / deno ;
-
   if (abs(deno) < 1E-8){
     return 0;
   }
