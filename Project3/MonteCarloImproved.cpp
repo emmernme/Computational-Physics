b68e665db859c59bc1b6c21cd14d7cc7bfea8fe0
Emil <emil.broll@gmail.com>
2019-10-17 11:20:06 +0200

    Trying out MonteCarlo

--- Project3/MonteCarloImproved.cpp
+++ Project3/MonteCarloImproved.cpp
@@ -6,44 +6,32 @@ Solving the integral in project 3 using Monte Carlo (MC) random method
 #include <fstream>
 #include <iomanip>
 #include <random>
-//#include "matplotlibcpp.h"
-//namespace plt = matplotlibcpp;
-
 
 // Constants
 #define _USE_MATH_DEFINES // Pi
-double alpha = 2.0;
+double alpha = 2;
 double exact = 5*M_PI*M_PI / (16*16);
 // Approximate ∞ (lambda)
-double lim = 0.345; // 0.445
+double max_lim = 0.445;
+double min_lim = -max_lim;
 // Number of MC samples
-int N = 1e7;
-// Tolerance to avoid r1-r2=0 (division by zero)
-double tol = 1e-9;
+int N = 1000000;
 
 using namespace std;
 
-void MonteCarlo(double lim);
+void MonteCarloImproved();
 double integration_func(double r1[], double r2[]);
 
 int main(){
-	/*MonteCarlo(lim);
-	MonteCarlo(1);
-	MonteCarlo(2);
-	MonteCarlo(3);*/
-	MonteCarlo(3);
+	MonteCarloImproved();
 	return 1;
 }
-// Calculate length of r-vector
-double abs_r(double r[]){
-	return sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
-}
 
-void MonteCarlo(double lim){
+void MonteCarloImproved(){
 	// Random number generator from C++11
 	random_device seeder;
 	mt19937 engine(seeder());
-	uniform_real_distribution<double> dist(0, lim);
+	uniform_real_distribution<double> dist(min_lim, max_lim);
 
 	// Set up integral vars
 	double MC_integral = 0;
@@ -70,21 +58,15 @@ void MonteCarlo(double lim){
 	// Calculate variance
 	double variance = MC_integral_squared - MC_integral*MC_integral;
 
-	cout << "Lim: " << lim << ", Variance: " << variance << ", integral: " << MC_integral << ", exact: " << exact << endl;
+	cout << "Variance: " << variance << ", integral: " << MC_integral << ", exact: " << exact << endl;
 }
 
-// Calculate distance between r-vectors
-double dist_r(double r1[], double r2[]){
-	return sqrt( pow(r1[0]-r2[0], 2) + pow(r1[1]-r2[1], 2) + pow(r1[2]-r2[2], 2) );
+// Calculate length of r-vector
+double abs_r(double r[]){
+	return sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
 }
 
 // The function to be integrated
 double integration_func(double r1[], double r2[]){
-	double num = exp (-2 * alpha * (sqrt(pow(r1[0], 2) + pow(r1[1], 2) + pow(r1[2], 2) + sqrt(pow(r2[0], 2) + pow(r2[1], 2) + pow(r2[2], 2)))));
-
-	double dist = abs(sqrt(pow(r1[0]-r2[0], 2) + pow(r1[0])) // dist_r(r1, r2);
-	return num / dist;
-
-	if (dist < tol) return 0; 
-	return exp(- (abs_r(r1) )); //+ abs_r(r2))); // / dist;
+	return exp(-alpha * (abs_r(r1) + abs_r(r2)));
 }
