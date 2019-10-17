6bb208335e3196bc13380f987456f179d903ea0a
Fredrik <fredrik1997@gmail.com>
2019-10-17 11:16:43 +0200

    plot og bugfix

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -7,16 +7,16 @@
 #include "gauss_laguerre.cpp"
 #include "int_function_polar.cpp"
 
-#define ZERO 1.0e-10
-
 using namespace std;
 
 
 int main()
 {
-  int N = 15; //number of integration points
+  int N = 10; //number of integration points
   double a = -5;
   double b = 5; //intergration limits
+
+  clock_t start, finish;
 
   int alpha = 2;
   double      const  pi = 3.14159265359;
@@ -46,7 +46,9 @@ int main()
 
   //Evaluate the integral with the Gauss-Legendre method
   double int_gauss = 0.;
-/*
+
+  start = clock();
+
   for (int i=0; i < N; i++){
       for (int j = 0; j<N; j++){
       for (int k = 0; k<N; k++){
@@ -58,10 +60,16 @@ int main()
   int_gauss += w[i]*w[j]*w[k]*w[l]*w[m]*w[n]*int_function(x[i], x[j], x[k], x[l], x[m], x[n]);
     }}}}}
   }
-*/
+
+  finish = clock();
+
+  double t1 = (double (finish - start))/CLOCKS_PER_SEC;
+
   //evaluate the intergrak with the Gauss-Laguerre method
   // we initialize the sum
   double int_gausslag = 0.;
+
+  start = clock();
   for (int i=1; i < N; i++){
         for (int j = 1; j<N; j++){
         for (int k = 0; k<N; k++){
@@ -71,9 +79,20 @@ int main()
     int_gausslag += w_r[i]*w_r[j]*w_t[k]*w_t[l]*w_p[m]*w_p[n]*int_function_polar(r[i], r[j], theta[k], theta[l], phi[m], phi[n]);
     }}}}}
 }
-  cout << "Exact value = 0.192765" << endl;
-  cout << "Gaussian Legendre quadrature = " << setw(20) << setprecision(15) << int_gauss << endl;
-  cout << "Gaussian Laguerre quadrature = " << setw(15) << setprecision(15) << int_gausslag << endl;
+  finish = clock();
+  double t2 = (double (finish - start))/CLOCKS_PER_SEC;
+
+
+  double exact = 0.192765;
+  double diff1 = fabs(int_gauss - exact);
+  double diff2 = fabs(int_gausslag - exact);
+
+
+  cout << "Exact value: " << setw(26) << 0.192765 << endl;
+  cout << "Gaussian Legendre quadrature = " << fixed << setprecision(6) << int_gauss;
+  cout << "   Diff: " << diff1 << "  Time taken: " << t1 << endl;
+  cout << "Gaussian Laguerre quadrature = " << setprecision(6) << int_gausslag;
+  cout << "   Diff: " << diff2 << "  Time taken: " << t2 << endl;
 
   delete [] x;
   delete [] w;
