905561044d5f5465605fa8c8afb9a87f134f1737
Emil <emil.broll@gmail.com>
2019-11-07 10:38:50 +0100

    LOL

--- Project4/Ising.cpp
+++ Project4/Ising.cpp
@@ -1,9 +1,15 @@
+// Compile using:
+// g++-9 -std=c++11 Ising.cpp -o Ising.o -larmadillo -O3 -lpthread -fopenmp
+
 #include <cmath>
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <random>
 #include <armadillo>
+// Requirements: OpenMP (brew install libomp clang-omp)
+#include <omp.h>
+
 using namespace std;
 using namespace arma; // Unwraps Armadillo-functions
 
@@ -12,7 +18,7 @@ double beta = 1; //  / (k_b * T);
 double J = 1; // Energy scale ?
 double k_b = 1; // Boltzmann scale ?
 
-int L = 5; // Dimension
+int L = 2; // Dimension
 double T = 1.0; // [kT/J]
 
 // Initialise random
@@ -21,7 +27,7 @@ mt19937 engine(seeder());
 uniform_real_distribution<double> dist(0, 1);
 uniform_real_distribution<double> pos(0, L-1);
 
-void IsingSetup();
+double IsingSetup(int n, bool random);
 void printMat(mat a);
 int randomSpin();
 int randomPos();
@@ -65,18 +71,23 @@ double E(mat spins, int x, int y){
 	return -E_count;
 }
 
-void IsingSetup(){
-	int N = 50; // Number of MC cycles
+double IsingSetup(int n, bool random){
+	int N = n; // Number of MC cycles
 
 	mat spins = mat(L,L);
-	// Set up initial spins randomly
-	for (int i = 0; i < L; i++){
-		for (int j = 0; j < L; j++){
-			int spin = randomSpin();
-			spins(i,j) = spin;
+	if (random){
+		// Set up initial spins randomly
+		for (int i = 0; i < L; i++){
+			for (int j = 0; j < L; j++){
+				int spin = randomSpin();
+				spins(i,j) = spin;
+			}
 		}
+	} else {
+		// Set all spins to +1
+		spins.fill(1);
 	}
-	printMat(spins);
+	//printMat(spins);
 
 	// Set up an array of possible \Delta E-values
 	int delta_E[] = {-8, -4, 0, 4, 8};
@@ -90,47 +101,37 @@ void IsingSetup(){
 	double M_abs_sum = 0.0;
 
 	// Monte Carlo-loop
+	#pragma omp parallel
 	for (int i = 0; i < N; i++){
-		int x = randomPos();
-		int y = randomPos();
+		int x = randomPos(), y = randomPos(); // Random position in the lattice
 		double r = dist(engine); // Random probability of flipping
 
-		// Metropolis algo
-		mat new_spins = spins;
-		new_spins(x,y) *= -1;
-		//double E_new = E(new_spins, x, y);
 		double E_old = E(spins, x, y);
-		double E_new = -E_old;
+		double E_new = -E_old; // Flipping the center spin always inverses the energy
+
+		// double P_ratio = exp(-beta * (E_new - E_old));
 		double P_ratio;
-
-		//cout << "E_new: " << E_new << ", E_old: " << E_old << endl;
-
-
-		//double P_ratio = exp(-beta * (E_new - E_old));
-
 		if (E_old == -4){
 			P_ratio = E_trans[0];
-		}
-		else if (E_old == 4){;
+		} else if (E_old == 4){;
 			P_ratio = E_trans[1];
-		}
-		else if (E_old == 0){
+		} else if (E_old == 0){
 			P_ratio = E_trans[2];
-		}
-		else if (E_old == -2){
+		} else if (E_old == -2){
 			P_ratio = E_trans[3];
-		}
-		else if (E_old == 2){;
+		} else if (E_old == 2){;
 			P_ratio = E_trans[4];
 		}
 
-
+		// Metropolis algo
 		if (r <= P_ratio){
-			spins = new_spins;
+			spins(x,y) *= -1;
 			E_old = E_new;
 		}
 	}
-	printMat(spins);
+	//printMat(spins);
+
+	return E_tot(spins);
 }
 
 // Print matrix + the energy
@@ -154,9 +155,50 @@ int randomPos(){
 	return (int) (pos(engine) + 0.5);
 }
 
-
-// Run, boy, run!
 int main(){
-	IsingSetup();
+	int n;
+	//cout << "How many MC cycles?" << endl;
+	//cin >> n;
+	for (int n = 100; n < 100000; n += 100){
+		cout << IsingSetup(n, true) << " with n=" << n << endl;
+	}
 	return 0;
 }
+/* 
+Run boy run!
+This world is not made for you
+Run boy run!
+They're trying to catch you
+Run boy run!
+Running is a victory
+Run boy run!
+Beauty lies behind the hillsRun boy run!
+The sun will be guiding you
+Run boy run!
+They're dying to stop you
+Run boy run!
+This race is a prophecy
+Run boy run!
+Break out from society
+Tomorrow is another day
+And you won't have to hide away
+You'll be a man, boy!
+But for now it's time to run
+It's time to run!Run boy run!
+This ride is a journey to
+Run boy run!
+The secret inside of you
+Run boy run!
+This race is a prophecy
+Run boy run!
+And disappear in the treesTomorrow is another day
+And you won't have to hide away
+You'll be a man, boy!
+But for now it's time to run
+It's time to run!
+Tomorrow is another day
+And when the night fades away
+You'll be a man, boy!
+But for now it's time to run
+It's time to run!
+*/
