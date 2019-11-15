b996904c245febbd8655c2743a2540de0694f650
Fredrik <fredrik1997@gmail.com>
2019-11-15 10:19:46 +0100

    outfile formatering

--- Project4/phase_transitions.cpp
+++ Project4/phase_transitions.cpp
@@ -1,6 +1,7 @@
 // Compile using:
 // c++ phase_transitions.cpp -o phase_transitions.o -larmadillo -O3 -lomp -std=c++11
 #include "Ising.cpp"
+#include <iostream>
 
 // Requirements: OpenMP ("brew install libomp clang-omp" + "lomp" compiler flag)
 #include <omp.h>
@@ -14,34 +15,34 @@ int main(){
 	double Tmax = 2.3;
 	int n = 10;
 	double dT = (Tmax-T0)/(double) n;
-	int N = 5e6;
+	int N = 20;
 
 
-	double *T = new double[n];
+	vector<double> T;
 	int L[4] = {40, 60, 80, 100};
 
-	mat E_mean       (4, n-1, fill::zeros);
-	mat M_abs_mean   (4, n-1, fill::zeros);
-	mat specific_heat(4, n-1, fill::zeros);
-	mat suceptibility(4, n-1, fill::zeros);
+	mat E_mean       (4, n+1, fill::zeros);
+	mat M_abs_mean   (4, n+1, fill::zeros);
+	mat specific_heat(4, n+1, fill::zeros);
+	mat suceptibility(4, n+1, fill::zeros);
 
 	vector<int> E_count;
 	vector<int> flip_N;
 
-
-	for(int i = 0; i < n; i++){
-	T[i] = T0+i*dT;
+	for(int i = 0; i <= n; i++){
+		T.push_back(T0+i*dT);
 	}
 
 	for (int i = 0; i < 4; i++){
-		for (int k = 0; k < n-1; k++){
-			vector<double> results = MonteCarloIsing(N, true, T0, L, E_count, flip_N);
+		for (int k = 0; k <= n; k++){
+			vector<double> results = MonteCarloIsing(N, true, T[k], L[i]);
 			E_mean(i,k)        = results[0];
 			M_abs_mean(i,k)    = results[4];
 			specific_heat(i,k) = results[7];
 			suceptibility(i,k) = results[8];
 		}
 	}
+
 
 /*
 	vector<int> E_count;
@@ -88,16 +89,21 @@ int main(){
 	}
 */
 
-	cout << setw(20) << "E_mean" << setw(15) <<  "M_mean" <<  "M_abs_mean";
-	cout << setw(15) <<  "specific_heat" << setw(15) << "suceptibility" << endl;
-	for(int i = 0; i < 4; i++){
-	for(int k = 0; k < n-1; k++){
-		cout << "T: " <<  T[i] << setprecision(4) << setw(15) << E_mean(i,k) << setw(15);
-		cout << setprecision(4) << M_abs_mean(i,k) << setw(15);
-		cout << setprecision(4) << specific_heat(i,k);
-		cout << setw(15) << setprecision(4) << suceptibility(i,k) << endl;
+	ofstream values_output;
+	values_output.open("phase_transitions.dat");
+	values_output << endl << setw(20) << "E_mean"  << setw(15) <<  "M_abs_mean";
+	values_output << setw(15) <<  "specific_heat" << setw(15) << "suceptibility" << endl;
+	for(int k = 0; k <= n; k++){
+		values_output << "T: " <<  T[k] << endl;;
+		for(int i = 0; i < 4; i++){
+			values_output << "L: " << L[i] << setw(3) << setprecision(4) << setw(15) << E_mean(i,k) << setw(15);
+			values_output << setprecision(4) << M_abs_mean(i,k) << setw(15);
+			values_output << setprecision(4) << specific_heat(i,k);
+			values_output << setw(15) << setprecision(4) << suceptibility(i,k) << endl;
+		}
+		values_output << endl;
 	}
-	}
+	values_output.close();
 
 return 0;
 }
