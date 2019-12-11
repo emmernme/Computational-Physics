d39fa6672a933b01e470a83ccb2663f97773b2c8
Emil <emil.broll@gmail.com>
2019-12-11 15:28:09 +0100

    Preparing for more planets

--- Project5/MEGABRAIN/simulator.cpp
+++ Project5/MEGABRAIN/simulator.cpp
@@ -24,12 +24,14 @@ int main (int argv, char **argc){
 	//Planet Sun("Sun", dim, 1, 0, 0, 0, 0, 0, 0);
 	//Planet Earth("Earth", dim, 0.00003, 1, 0, 0, 0, 6.3, 0);
 	Planet Sun("Sun", dim, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-	Planet Earth("Earth", dim, 0.00003, 1.0, 0.0, 0.0, 0.0, 2*M_PI, 0.1);
+	Planet Earth("Earth", dim, 0.00003, 1.0, 0.0, 0.0, 0.0, 2*M_PI, 0);
+	Planet Jupiter("Jupiter", dim, 0.00095, 5.2, 0.0, 0.0, 0.0, 10*M_PI, 0.1);
 
 	// Set up the Solar system
 	System Solar_system(10.0);
 	Solar_system.add_planet(Sun);
 	Solar_system.add_planet(Earth);
+	Solar_system.add_planet(Jupiter);
 
 	// Simulate the passing of time
 	Solar_system.VelocityVerlet(dim, N, years);
