993f2900f7e56e3f22b363f6a9e4c10f216b070c
Emil <emil.broll@gmail.com>
2019-12-12 13:50:43 +0100

    Update system_plot.py

--- Project5/MEGABRAIN/system_plot.py
+++ Project5/MEGABRAIN/system_plot.py
@@ -8,47 +8,31 @@ infile = open(sys.argv[1], 'r')
 infile.readline()
 info = infile.readline()
 
-file2 = open("Vel 2_5.data", 'r')
-file2.readline()
+# Get the basic info about the system
+info = info.split(',')
+dim = int(info[0])
+n = int(info[1])
+years = float(info[2])
+dt = float(info[3])
+radius = float(info[4])
+planet_count = int(info[5])
 
-file3 = open("Vel 2_82.data", 'r')
-file3.readline()
+infile.readline()
 
-file4 = open("Vel 2_83.data", 'r')
-file4.readline()
+# Get info about and prepare the planets
+planets = []
+for i in range(planet_count):
+    planet = infile.readline().split(",")
+    planets.append({"name": planet[1].strip(), "position": np.zeros((n-1, dim))})
 
+infile.readline()
 
-files = [file1, file2, file3, file4]
-systems = []
-for infile in files:
-    info = infile.readline()
-    # Get the basic info about the system
-    info = info.split(',')
-    dim = int(info[0])
-    n = int(info[1])
-    years = float(info[2])
-    dt = float(info[3])
-    radius = float(info[4])
-    planet_count = int(info[5])
-
-    infile.readline()
-
-    # Get info about and prepare the planets
-    planets = []
-    for i in range(planet_count):
-        planet = infile.readline().split(",")
-        planets.append({"name": planet[1].strip(), "position": np.zeros((n-1, dim))})
-
-    infile.readline()
-
-    # Get the positions at each time step
-    for i in range(n-1):
-        # Get the position for each planet
-        for j in range(planet_count):
-            pos = infile.readline().split('=')[1].split(',')
-            planets[j]["position"][i] = [float(x.strip()) for x in pos]
-
-    systems.append(planets)
+# Get the positions at each time step
+for i in range(n-1):
+    # Get the position for each planet
+    for j in range(planet_count):
+        pos = infile.readline().split('=')[1].split(',')
+        planets[j]["position"][i] = [float(x.strip()) for x in pos]
 
 
 plt.style.use("dark_background")
@@ -61,21 +45,21 @@ ax.yaxis.set_pane_color((0, 0, 0, 1.0))
 ax.zaxis.set_pane_color((0, 0, 0, 1.0))
 ax.grid(b=False)
 
-for planets in systems:
-    for i in range(planet_count):
-        pos = planets[i]["position"]
-        if (planets[i]["name"] == "Sun"):
-            if (dim == 3):
-                ax.scatter([0], [0], [0], label=planets[i]["name"], color="#ccff00", linewidth=10)
-            else:
-                ax.scatter([x[0] for x in pos], [x[1] for x in pos], label=planets[i]["name"], color="#ccff00", linewidth=10)
 
+for i in range(planet_count):
+    pos = planets[i]["position"]
+    if (planets[i]["name"] == "Sun"):
+        if (dim == 3):
+            #ax.scatter([x[0] for x in pos], [x[1] for x in pos], [x[2] for x in pos], label=planets[i]["name"], color="#ccff00", linewidth=10)
+            ax.scatter([0],[0],[0], label=planets[i]["name"], color="#ccff00", linewidth=10)
         else:
-            if (dim == 3):
-                ax.plot([x[0] for x in pos], [x[1] for x in pos], [x[2] for x in pos], label=planets[i]["name"])
-            else:
-                ax.plot([x[0] for x in pos], [x[1] for x in pos], label=planets[i]["name"])
-    
+            ax.scatter([x[0] for x in pos], [x[1] for x in pos], label=planets[i]["name"], color="#ccff00", linewidth=10)
+
+    else:
+        if (dim == 3):
+            ax.plot([x[0] for x in pos], [x[1] for x in pos], [x[2] for x in pos], label=planets[i]["name"])
+        else:
+            ax.plot([x[0] for x in pos], [x[1] for x in pos], label=planets[i]["name"])
 
 
 ax.legend()
